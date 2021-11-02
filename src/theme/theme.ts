bd6a85073b0457cc44a436e53d910878dd37dcad
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-11-01 21:45:30 -0300

    start and configuring

---
+++ src/theme/theme.ts
@@ -0,0 +1,30 @@
+export const colors = {
+	red: "#7d3a3c",
+	blue: "#394165",
+	grey: {
+		light: "#f2f3f5",
+		medium: "#4d4c4e",
+		dark: "#2B2B2B",
+		darkest: "#1b1b1b",
+	},
+	black: "#000000",
+	white: "#ffffff",
+
+	facebook: "#3b5998",
+	linkedin: "#007AB9",
+	twitter: "#55ACEE",
+	instagram:
+		"radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)",
+
+	lightBlueGradient: "#linear-gradient(to right, #60B6E5 0%, rgba(96, 182, 229, 0.3) 100%);",
+};
+
+export const light = {
+	text: colors.grey.dark,
+	backgroundColor: colors.white,
+};
+
+export const dark = {
+	text: colors.white,
+	backgroundColor: colors.grey.darkest,
+};
