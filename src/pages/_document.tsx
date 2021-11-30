578bf7f07add0773d85dce42755ec063a75a4c84
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-11-29 21:11:55 -0300

    feat(hotjar): add hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable */
+/* tslint:disable */
+import { useEffect, VoidFunctionComponent } from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import { hotjar } from "react-hotjar";
 
 export default class MyDocument extends Document {
 	render() {
@@ -6,10 +11,7 @@ export default class MyDocument extends Document {
 			<Html lang="pt-br">
 				<Head>
 					<meta charSet="UTF-8" />
-					<meta
-						name="description"
-						content="Gestotus Consultoria e treinamentos Alagoas Brasil"
-					/>
+					<meta name="description" content="Gestotus Consultoria e Treinamentos" />
 					<link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="true" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link
@@ -17,6 +19,25 @@ export default class MyDocument extends Document {
 						rel="stylesheet"
 					/>
 					<link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+					{/* {(function (h, o, t, j, a, r) {
+						h.hj =
+							h.hj ||
+							function () {
+								(h.hj.q = h.hj.q || []).push(arguments);
+							};
+						h._hjSettings = { hjid: 2724893, hjsv: 6 };
+						a = o.getElementsByTagName("head")[0];
+						r = o.createElement("script");
+						r.async = 1;
+						r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+						a.appendChild(r);
+					})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=")} */}
+
+					{useEffect(() => {
+						hotjar.initialize(2724893, 6);
+						// window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=";
+					}, [])}
 				</Head>
 				<body>
 					<Main />
