bca7c49a9d330be23c30a90b5f63f0601b359c37
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2021-12-10 22:57:00 -0300

    feat(activeNavBar): add active class

--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -1,35 +1,47 @@
 import Link from "next/link";
-
+import { useRouter } from "next/router";
 import styles from "./styles.module.scss";
 
-export const Nav = () => (
-	<nav className={styles.menu}>
-		<ul>
-			<li>
-				<Link href="/" passHref replace>
-					<a>Home</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/#cursos" passHref scroll={false} replace>
-					<a>Cursos</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/blog" passHref scroll={false} replace>
-					<a>Blog</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/drlicitacao" passHref replace>
-					<a>Dr. Licitação</a>
-				</Link>
-			</li>
-			<li>
-				<Link href="/about" passHref replace>
-					<a>Sobre Nós</a>
-				</Link>
-			</li>
-		</ul>
-	</nav>
-);
+export const Nav = () => {
+	const router = useRouter();
+
+	console.log(router.pathname);
+
+	return (
+		<nav className={styles.menu}>
+			<ul>
+				<li>
+					<Link href="/" passHref replace>
+						<a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/#cursos" passHref scroll={false} replace>
+						<a className={router.pathname === "/#cursos" ? styles.active : ""}>
+							Cursos
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/blog" passHref scroll={false} replace>
+						<a className={router.pathname === "/blog" ? styles.active : ""}>Blog</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/drlicitacao" passHref replace>
+						<a className={router.pathname === "/drlicitacao" ? styles.active : ""}>
+							Dr. Licitação
+						</a>
+					</Link>
+				</li>
+				<li>
+					<Link href="/about" passHref replace>
+						<a className={router.pathname === "/about" ? styles.active : ""}>
+							Sobre Nós
+						</a>
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
